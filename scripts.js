a64d9cb5db4d1f510424c578b66e9786199ad455
rutkovzky <55913301+rutkovzky@users.noreply.github.com>
2019-10-12 19:12:31 +0200

    fixes #2 added file download (#10)

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 console.log('scripts loaded')
+
+var storedText;
+
+var url = 'https://s3.zylowski.net/public/input/8.txt';
+
+document.getElementById("1").onclick = function () {
+    fetch(url)
+        .then(function (response) {
+            response.text().then(function (text) {
+                storedText = text;
+                done();
+            });
+        });
+
+    function done() {
+        document.getElementById('log').textContent =
+            "file loaded";
+    }
+};
