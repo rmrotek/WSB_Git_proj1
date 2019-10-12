3690f5837f5289e3f5176caccc25bc78c35c3c9f
rmrotek <remek87@gmail.com>
2019-10-12 19:19:52 +0200

    fixes #3 char count added

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 console.log('scripts loaded')
 
 var storedText;
-
+var log = document.getElementById('log')
 var url = 'https://s3.zylowski.net/public/input/8.txt';
 
 document.getElementById("1").onclick = function () {
@@ -14,7 +14,16 @@ document.getElementById("1").onclick = function () {
         });
 
     function done() {
-        document.getElementById('log').textContent =
-            "file loaded";
+        log.textContent =
+            "Plik wczytany";
     }
+};
+
+document.getElementById("2").onclick = function () {
+    if (!!storedText) {
+        log.textContent = 'Liczba znakow to: ' + storedText.length
+    } else {
+        log.textContent = 'Brak pliku!'
+    }
+    
 };
