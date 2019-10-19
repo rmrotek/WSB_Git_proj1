915a2c9c3a647377c8a0f44cacc3683ef2ea77b2
rmrotek <remek87@gmail.com>
2019-10-19 12:29:26 +0200

    fixes #19 - sentence count change

--- scripts.js
+++ scripts.js
@@ -50,7 +50,7 @@ document.getElementById("4").onclick = function () {
 
 document.getElementById("5").onclick = function () {
     if (!!storedText) {
-        log.textContent = 'Liczba zdań to: ' + (storedText.match(/[\w|\)][.?!](\s|$)/g) || []).length
+        log.textContent = 'Liczba zdań to: ' + (storedText.match(/[\w|\)][.?](\s|$)/g) || []).length
     } else {
         log.textContent = 'Brak pliku!'
     }
