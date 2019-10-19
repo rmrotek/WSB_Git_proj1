c1f7767fa2c67dd1f3d9c0bd695e4e358669f3b5
rmrotek <remek87@gmail.com>
2019-10-19 12:21:17 +0200

    fixes #17 - word count change

--- scripts.js
+++ scripts.js
@@ -5,78 +5,76 @@ var log = document.getElementById('log')
 var url = 'https://s3.zylowski.net/public/input/8.txt';
 
 document.getElementById("1").onclick = function () {
-    fetch(url)
-        .then(function (response) {
-            response.text().then(function (text) {
-                storedText = text;
-                done();
-            });
-        });
+  fetch(url)
+    .then(function (response) {
+      response.text().then(function (text) {
+        storedText = text;
+        done();
+      });
+    });
 
-    function done() {
-        log.textContent =
-            "Plik wczytany";
-    }
+  function done() {
+    log.textContent =
+      "Plik wczytany";
+  }
 };
 
 document.getElementById("2").onclick = function () {
-    if (!!storedText) {
-        log.textContent = 'Liczba znakow to: ' + storedText.length
-    } else {
-        log.textContent = 'Brak pliku!'
-    }
+  if (!!storedText) {
+    log.textContent = 'Liczba znakow to: ' + storedText.length
+  } else {
+    log.textContent = 'Brak pliku!'
+  }
 
 };
 
 document.getElementById("3").onclick = function () {
-    if (!!storedText) {
-        log.textContent = 'Liczba wyrazów to: ' + storedText.split(' ')
-            .filter(function (n) { return n != '' })
-            .length;
-    } else {
-        log.textContent = 'Brak pliku!'
-    }
-
+  if (!!storedText) {
+    var words = storedText.split(' ').filter(function (n) { return n != '' && n.length > 1 }).length
+    log.textContent = 'Liczba wyrazów to: ' + words;
+  } else {
+    log.textContent = 'Brak pliku!'
+  }
 };
 
 document.getElementById("4").onclick = function () {
-    if (!!storedText) {
-        log.textContent = 'Liczba znaków interpunkcyjnych to: ' + (storedText.match(/[,.!@#$%^&*()]/g) || []).length;
-    } else {
-        log.textContent = 'Brak pliku!'
-    }
+  if (!!storedText) {
+    log.textContent = 'Liczba znaków interpunkcyjnych to: ' + (storedText.match(/[,.!@#$%^&*()]/g) || []).length;
+  } else {
+    log.textContent = 'Brak pliku!'
+  }
 
 };
 
 document.getElementById("5").onclick = function () {
-    if (!!storedText) {
-        log.textContent = 'Liczba zdań to: ' + (storedText.match(/[\w|\)][.?!](\s|$)/g) || []).length
-    } else {
-        log.textContent = 'Brak pliku!'
-    }
+  if (!!storedText) {
+    log.textContent = 'Liczba zdań to: ' + (storedText.match(/[\w|\)][.?!](\s|$)/g) || []).length
+  } else {
+    log.textContent = 'Brak pliku!'
+  }
 
 };
 
 document.getElementById("6").onclick = function () {
-    if (!!storedText) {
-        var counts = {}
-        var ch, index, len, count;
-        log.textContent = ''
-        for (index = 0, len = storedText.length; index < len; ++index) {
-            ch = storedText.charAt(index); 
-            count = counts[ch];
-            counts[ch] = count ? count + 1 : 1;
-        }
-        for (char in counts) {
-            log.insertAdjacentHTML("beforeend",`${char}: ${counts[char]} <br>` ) 
-        }
-        
-    } else {
-        log.textContent = 'Brak pliku!'
+  if (!!storedText) {
+    var counts = {}
+    var ch, index, len, count;
+    log.textContent = ''
+    for (index = 0, len = storedText.length; index < len; ++index) {
+      ch = storedText.charAt(index);
+      count = counts[ch];
+      counts[ch] = count ? count + 1 : 1;
     }
+    for (char in counts) {
+      log.insertAdjacentHTML("beforeend", `${char}: ${counts[char]} <br>`)
+    }
+
+  } else {
+    log.textContent = 'Brak pliku!'
+  }
 
 };
 
 document.getElementById("8").onclick = function () {
-    window.close();
+  window.close();
 }
